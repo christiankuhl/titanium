00488ef4e379f6acd99df4c3568b9b9d450df509
Christian Kuhl <christian.kuhl84@gmail.com>
2021-08-23 22:09:02 +0200

    Fix debug blunder...

--- toolchain/src/debug.rs
+++ toolchain/src/debug.rs
@@ -5,5 +5,8 @@ fn main() {
     Command::new("cargo").arg("build").status().expect("Build failed!");
     let kernel_binary = "../target/x86_64-titanium/debug/titanium";
     toolchain::build_bootimage(kernel_binary, false);
-    toolchain::start_qemu(kernel_binary, false, true, args())
+    let mut add_args = args();
+    add_args.next();
+    add_args.next();
+    toolchain::start_qemu(kernel_binary, false, true, add_args)
 }
