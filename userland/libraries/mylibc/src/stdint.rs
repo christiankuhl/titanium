af6516a2a9628396190f0b5a5201239ad8a010d8
Christian Kuhl <christian.kuhl84@gmail.com>
2021-08-26 03:55:23 +0200

    libc stub
    This is a stub for a soon to be libc implementation

--- userland/libraries/mylibc/src/stdint.rs
+++ userland/libraries/mylibc/src/stdint.rs
@@ -15,8 +15,8 @@ pub type int_least16_t = int16_t;
 pub type int_least32_t = int32_t;
 pub type int_least64_t = int64_t;
 pub type uint_least8_t = uint8_t;
-pub type uint_least16_t = uint16_t; 
-pub type uint_least32_t = uint32_t; 
+pub type uint_least16_t = uint16_t;
+pub type uint_least32_t = uint32_t;
 pub type uint_least64_t = uint64_t;
 
 pub type int_fast8_t = int8_t;
@@ -24,8 +24,8 @@ pub type int_fast16_t = int16_t;
 pub type int_fast32_t = int32_t;
 pub type int_fast64_t = int64_t;
 pub type uint_fast8_t = uint8_t;
-pub type uint_fast16_t = uint16_t; 
-pub type uint_fast32_t = uint32_t; 
+pub type uint_fast16_t = uint16_t;
+pub type uint_fast32_t = uint32_t;
 pub type uint_fast64_t = uint64_t;
 
 pub type intptr_t = isize;
@@ -33,4 +33,3 @@ pub type uintptr_t = usize;
 
 pub type intmax_t = i128;
 pub type uintmax_t = u128;
-
